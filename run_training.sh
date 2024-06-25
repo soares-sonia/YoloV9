0b59a54e60940e0ad3abd6e3ef3a30fe0c5df76e
Sonia Soares <soniasoares505@gmail.com>
2024-06-25 23:32:05 +0100

    Run Training Command

---
+++ run_training.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Variables (update these paths according to your setup)
+YOLOV9_DIR="/yolov9"
+DATASET_LOCATION="/bananas_dataset"
+WEIGHTS_PATH="/weights/yolov9-e.pt"
+CFG_PATH="models/detect/yolov9-e.yaml"
+HYP_PATH="hyp.scratch-high.yaml"
+
+# Navigate to the YOLOv9 directory
+echo "Navigating to YOLOv9 directory..."
+cd $YOLOV9_DIR
+
+# Run the training command
+echo "Running the training script..."
+python train.py \
+  --batch 16 \
+  --epochs 25 \
+  --img 640 \
+  --device 0 \
+  --min-items 0 \
+  --close-mosaic 15 \
+  --data $DATASET_LOCATION/data.yaml \
+  --weights $WEIGHTS_PATH \
+  --cfg $CFG_PATH \
+  --hyp $HYP_PATH
+
+echo "Training script finished."
