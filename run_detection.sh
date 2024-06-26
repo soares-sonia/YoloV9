2203344dc023641f17c2946465b04161f76d5019
Sonia Soares <soniasoares505@gmail.com>
2024-06-26 14:09:09 +0100

    Fix Paths

--- run_detection.sh
+++ run_detection.sh
@@ -2,7 +2,7 @@
 
 # Check if model path and capture mode are provided
 if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Usage: ./yolov9/run_detection.sh <path_to_model> <screen/webcam>"
+  echo "Usage: ./run_detection.sh <path_to_model> <screen/webcam>"
   exit 1
 fi
 
@@ -15,4 +15,4 @@ source yolov9_env/bin/activate
 
 # Run the screen capture detection script with the model path and capture mode
 echo "Running screen capture detection..."
-python yolov9/real_time_detection.py $MODEL_PATH $DETECT
+python ./real_time_detection.py $MODEL_PATH $DETECT
